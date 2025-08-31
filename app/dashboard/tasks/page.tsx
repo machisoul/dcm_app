aac9a4d5917bb0fb923cc24993f3006d016742a4
vidge <vidge@machisoul.com>
2025-08-31 17:20:22 +0800

    add task page

---
+++ app/dashboard/tasks/page.tsx
@@ -0,0 +1,31 @@
+import { promises as fs } from "fs";
+import path from "path";
+import { z } from "zod";
+
+import { columns } from "@/components/columns";
+import { DataTable } from "@/app/dashboard/tasks/data-table";
+import { taskSchema } from "@/app/dashboard/tasks/data/schema";
+
+async function getTasks() {
+  const data = await fs.readFile(path.join(process.cwd(), "app/dashboard/tasks/data/tasks.json"));
+
+  const tasks = JSON.parse(data.toString());
+
+  return z.array(taskSchema).parse(tasks);
+}
+
+export default async function TaskPage() {
+  const tasks = await getTasks();
+
+  return (
+    <div className="h-full flex-1 flex-col gap-8 p-8 md:flex">
+      <div className="flex items-center justify-between gap-2">
+        <div className="flex flex-col gap-1">
+          <h2 className="text-2xl font-semibold tracking-tight">Welcome back!</h2>
+          <p className="text-muted-foreground">Here&apos;s a list of your tasks for this month.</p>
+        </div>
+      </div>
+      <DataTable data={tasks} columns={columns} />
+    </div>
+  );
+}
